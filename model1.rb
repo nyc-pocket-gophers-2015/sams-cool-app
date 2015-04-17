5b4b41d526274596801afb81e88d4433500bbe19
Sam Blackman <email": null>
2015-04-17 10:47:06 -0400

    worked on the dog

--- model1.rb
+++ model1.rb
@@ -1,5 +1,8 @@
 class Dog
- def initialize()
-
- end
+  attr_accessor :
+  def initialize(name, breed)
+    @name = name
+    @breed = breed
+    # let's turn this into an argument order independent hash later...
+  end
 end
